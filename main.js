b482d31ffbb6034b3f6f06d6d0ee316937438101
alrago24 <alinson_04@hotmail.com>
2024-11-22 09:48:42 -0500

    Segunda fusion del proyecto donde integramos el mobile-menu con el icono de menu hamburguesa, adcional le agregamos media queryes tanto para el mobile-menu como para el desktop-menu

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+const burguerMenu = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
 
 menuEmail.addEventListener('click', toggleDesktopMenu)
+burguerMenu.addEventListener('click', togglemobileMenu)
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
 }
 
-console.log("JS funcionando");
+function togglemobileMenu() {
+    mobileMenu.classList.toggle('inactive');
+}
